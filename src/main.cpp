97d620cf5009090c3cd94e7379d8a6dd48d95852
Dennis <denniscastrillon@gmail.com>
2021-04-27 11:11:17 -0500

    Codigo de sensores analogicos, digitales y LM35

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <Arduino.h>
 
+#include <sensors/analog_sensor.h>
+#include <sensors/digital_sensor.h>
+#include <sensors/lm35_sensors.h>
 
-
+uint8_t source_lm35 = A0;
+AnalogSensor sensor = AnalogSensor(0x7E, source_lm35);
+Lm35Sensors sensor_lm35 = Lm35Sensors(13, source_lm35);
 
 
 void setup() {
@@ -9,7 +14,14 @@ void setup() {
 }
 
 void loop() {
-
+sensor.excecute();
+    sensor_lm35.excecute();
+    int raw_lm35= sensor_lm35.getValue().getValue();
+    int raw_read = sensor.getValue().getValue();
+    Serial.print(raw_read);
+    Serial.print("\n  ");
+    Serial.print(raw_lm35);
+    Serial.print("\n  ");
   
     
   
